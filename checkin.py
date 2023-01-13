76a0c422de86759c25ca08c5486e5ffad21e87aa
discodyer <cody23333@gmail.com>
2023-01-13 20:30:21 +0800

    适配2023寒假

--- checkin.py
+++ checkin.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python          
+# -*- encoding: utf-8 -*- 
+
 from requests import get, post
 from time import perf_counter, sleep
 import json
 import time
 import random
-import requests
 
 api_url = "http://fyxt.nytdc.edu.cn:12078/"
 
+your_tel = 114514
+your_id = 1919810
+
 def get_dname() -> str:
+    '''
+    @description : 获取学校名称
+    @return : 学校名称
+    '''
     name_json = get(url=api_url + "api/getDname?dguid=61").json()
     if (name_json["msg"]=="成功"):
         school_name = name_json["data"]["dname"]
@@ -18,6 +27,10 @@ def get_dname() -> str:
     return school_name
 
 def veri_user_token(user_token):
+    """
+    @description : 验证用户token
+    @return : Bool
+    """
     user_token_headers = {'x-user-token': user_token, 'x-requested-with': "XMLHttpRequest"}
     veri_response_json = get(url = api_url + "tApi/verifyToken", headers = user_token_headers).json()
     if veri_response_json["msg"] == "成功":
@@ -26,9 +39,12 @@ def veri_user_token(user_token):
     else:
         print("token验证失败")
         return False
-        
 
 def get_new_checkin(user_token):
+    """
+    @description : 获取上一次登记的数据
+    @return : 反序列化的json数据
+    """
     user_token_headers = {'x-user-token': user_token, 'x-requested-with': "XMLHttpRequest"}
     veri_response_json = get(url = api_url + "tApi/getNewCheckin", headers = user_token_headers).json()
     if veri_response_json["msg"] == "成功":
@@ -38,44 +54,83 @@ def get_new_checkin(user_token):
         print("getNewCheckin失败")
 
 def get_latest_checkin(user_token, uuid):
+    """
+    @description : 获取某一次登记的数据
+    @params : 
+        user_token : 用户token
+        uuid : 要查看的uuid
+    @return : 反序列化的json数据
+    """
     user_token_headers = {'x-user-token': user_token, 'x-requested-with': "XMLHttpRequest"}
     get_checkin_url = api_url + "tApi/getCheckin?unitguid=" + uuid 
     response_json = get(url = get_checkin_url, headers = user_token_headers).json()
     return response_json
 
-
-def auto_checkin(tel, id):
+def get_token(tel, id):
+    """
+    @description : 获取token
+    @params : 
+        tel : 手机号
+        id  : 学号
+    @return : user_token 用户token
+    """
     if (get_dname() == "南邮通达学院"):
         stu_url = api_url + "api/login?mobile="+ str(tel) + "&code=" + str(id) + "&openid=&dguid=61"
         stu_json = get(url=stu_url).json()
         user_token = stu_json["data"]["token"]
-        #print("Token:"+user_token)
-        veri_user_token(user_token)
+        return user_token
+
+def auto_checkin(tel, id):
+    '''
+    @description: 自动签到
+    @params :
+        tel : 手机号
+        id  : 学号
+    @return 是否签到成功
+    '''    
+    user_token = get_token(tel, id)
+    if(veri_user_token(user_token)):
         last_upload_json = get_new_checkin(user_token)
         print(str(last_upload_json))
-        veri_user_token(user_token)
+        # veri_user_token(user_token)
+        is_inoculation = lambda x: "否" if x=="未接种" else "是"
+        is_secondrecovery = lambda x: "否" if x=="未二次感染" else "是"
+        is_recovery = lambda x: "否" if x[0]=="未" else "是"
         checkin_list = {
             "date" : str(time.strftime("%Y-%m-%d", time.localtime())), 
-            "ishubei": "否",
-            "fromWhere":"江苏省扬州市邗江区",
-            "province":"江苏省",
-            "city":"扬州市",
-            "county":"邗江区",
+            "fromWhere": str(last_upload_json["data"]["province"]) + str(last_upload_json["data"]["city"]),
+            "province": str(last_upload_json["data"]["province"]),
+            "city": str(last_upload_json["data"]["city"]),
+            "county": str(last_upload_json["data"]["county"]),
             "tempnum1":"36." + str(random.randint(0,9)),
-            "tempnum2":"36." + str(random.randint(0,9)),
-            "tempnum3":"36." + str(random.randint(0,9)),
-            "isgohubei":"否",
-            "iscontacthubei":"否",
-            "iscontactpatient":"否",
-            "istemp":"否",
-            "isfamilypatient":"否",
-            "isabroad":"否",
-            "isfamilyhubei":"正常"
+            "isinoculation": str(last_upload_json["data"]["isinoculation"]),
+            "isinoculation1": str(is_inoculation(last_upload_json["data"]["inoculationtime1"])),
+            "isinoculation2": str(is_inoculation(last_upload_json["data"]["inoculationtime2"])),
+            "issecondrecovery": str(is_secondrecovery(last_upload_json["data"]["secondrecoverytime"])),
+            "isinfectcovid": str(last_upload_json["data"]["isinfectcovid"]),
+            "isrecovery": str(is_recovery(last_upload_json["data"]["recoverytime"])),
+            "issecondinfect": str(last_upload_json["data"]["issecondinfect"]),
+            "isinoculation3": str(is_inoculation(last_upload_json["data"]["inoculationtime3"])),
+            "isinoculation4": str(is_inoculation(last_upload_json["data"]["inoculationtime4"])),
+            "isbasedisease": str(last_upload_json["data"]["isbasedisease"]),
+            "inoculationtype1": str(last_upload_json["data"]["inoculationtype1"]),
+            "inoculationtype2": str(last_upload_json["data"]["inoculationtype2"]),
+            "inoculationtype3": str(last_upload_json["data"]["inoculationtype3"]),
+            "inoculationtype4": str(last_upload_json["data"]["inoculationtype4"]),
+            "inoculationtime1": str(last_upload_json["data"]["inoculationtime1"]),
+            "inoculationtime2": str(last_upload_json["data"]["inoculationtime2"]),
+            "inoculationtime3": str(last_upload_json["data"]["inoculationtime3"]),
+            "inoculationtime4": str(last_upload_json["data"]["inoculationtime4"]),
+            "infecttime": str(last_upload_json["data"]["infecttime"]),
+            "recoverytime": str(last_upload_json["data"]["recoverytime"]),
+            "secondinfecttime": str(last_upload_json["data"]["secondinfecttime"]),
+            "secondrecoverytime": str(last_upload_json["data"]["secondrecoverytime"]),
+            "istemp": str(last_upload_json["data"]["istemp"])
             }
+        
         print(checkin_list)
         post_headers = {'x-user-token': user_token, 'x-requested-with': "XMLHttpRequest"}
         respond_data_json = post(url = api_url + "tApi/postCheckin", data = checkin_list, headers = post_headers).json()
-        # requests.post(url = "http://127.0.0.1:3000/", data = checkin_list)
         data_uuid = respond_data_json["data"]
         veri_user_token(user_token)
         latest_checkin_json = get_latest_checkin(user_token, data_uuid)
@@ -86,5 +141,5 @@ def auto_checkin(tel, id):
         else:
             return False
 
-if auto_checkin(__这边填你的手机号__, __这边填你的学号__):
-    print("签到成功")
+if(auto_checkin(your_tel, your_id)):
+    print("签到成功！")
